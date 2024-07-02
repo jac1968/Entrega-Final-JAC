aa86e465628f0c7da0ace682b107724f5b5da7a0
Jesús Carrasquilla <jacvcarrasquilla@gmail.com>
2024-07-01 23:47:36 -0400

    8ma Actualización

--- src/tests/cart.test.js
+++ src/tests/cart.test.js
@@ -6,7 +6,11 @@ const Product = require('../models/Product')
 const BASE_URL = '/api/v1/cart'
 const BASE_URL_LOGIN = '/api/v1/users'
     
-let TOKEN, cart, cartId, product  
+let TOKEN, cart, cartId, product, userId
+
+afterAll(async () => {
+    await product.destroy()
+})
 
 beforeAll(async () => {
     
@@ -20,17 +24,20 @@ beforeAll(async () => {
         .send(login)
     
     TOKEN = res.body.token
+    userId = res.body.user.id
 
-    product = await Product.create({
-        title: 'Samsung S40',
-        description: 'Samsung S40 color negro mate',
-        price: 249.99
-    });
+    product= await Product.create(
+        {
+            title: 'Samsung S40',
+            description: 'Samsung S40 color negro mate',
+            price: 249.99
+        }
+    );
 
     cart = {
         quantity: 1,
         productId: product.id,
-        userId: res.body.id
+        userId: res.body.user.id
     }
     
 }) 
@@ -43,7 +50,7 @@ test("Post  👉  'BASE_URL',  must  return  status  201  &  res.body.quantity >
         .send(cart)
         .set('Authorization', `Bearer ${TOKEN}`)
 
-    const cartId = res.body.id
+    cartId = res.body.id
     
     expect(res.status).toBe(201)
     expect(res.body).toBeDefined()
@@ -61,8 +68,25 @@ test("Get 👉 'BASE_URL', must  return  status 200  & res.body.length == 1", as
     expect(res.status).toBe(200)
     expect(res.body).toBeDefined()
     expect(res.body).toHaveLength(1)
+    expect(res.body[0].userId).toBeDefined()
+    expect(res.body[0].product).toBeDefined()
     
 })
+
+
+test("Get 👉 'BASE_URL/:id', must return status 200 and res.body.quantity === cart.quantity", async () => {
+    
+    const res = await request(app)
+        .get(`${BASE_URL}/${cartId}`)
+        .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.quantity).toBe(cart.quantity)
+    expect(res.body.userId).toBeDefined()
+    expect(res.body.product).toBeDefined()
+}) 
+
 
  test("Put 👉 'BASE_URL/:id', must return status 200 & res.body.quantity === cartUpdate.quantity", async () => {
 
@@ -88,6 +112,5 @@ test("Delete 👉 'BASE_URL/:id', must return status 204", async () => {
         .set('Authorization', `Bearer ${TOKEN}`)
     
     expect(res.status).toBe(204)
-    
         
 }) 
